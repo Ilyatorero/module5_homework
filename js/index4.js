ec06814364481bd56439d408267704443331c665
Ilyatorero <86488807+Ilyatorero@users.noreply.github.com>
2021-10-18 23:17:10 +0300

    5 homework module

---
+++ js/index4.js
@@ -0,0 +1,7 @@
+/*Задание 4
+Записать в переменную случайное целое число в диапазоне [0; 100]. 
+Используйте объект Math.*/
+
+let numder1 = 0
+let number2 = 100
+console.log (Math.random() * (number2 - numder1) + numder1 )
